b5391a28499b7b8fd12c91d8cdeb1b3b6cec0c0e
Max Ohsawa <max.ohsawa@gmail.com>
2022-10-15 16:23:41 -0400

    increases rate limit to 1000 requests in 10 mins

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const app = express();
 // rate limiting
 const limiter = rateLimit({
   windowMs: 10 * 60 * 1000, // 10 mins
-  max: 5,
+  max: 1000,
 });
 app.use(limiter);
 app.set('trust proxy', 1);
